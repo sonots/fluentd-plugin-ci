c863ec17941deebf39d665eee4b2c86db521cf70
sonots <sonots@gmail.com>
2014-05-18 00:55:51 +0900

    with_clean_env

--- run-tests.rb
+++ run-tests.rb
@@ -5,11 +5,15 @@ specs = Gem::Specification.find_all { |s| s.name =~ /fluent-plugin/ }
 passes = specs.map do |spec|
   puts "\e[31m\e[43m\e[5mRunning tests for #{spec.name}\e[0m"
 
-  system <<"EOF"
+  Bundler.with_clean_env do
+    system <<"EOF"
 cd #{spec.full_gem_path}
-BUNDLE_GEMFILE=Gemfile bundle
-RUBYLIB=lib:test:$RUBYLIB BUNDLE_GEMFILE=Gemfile bundle exec rake
+echo "bundle install"
+bundle install
+echo "bundle exec rake"
+bundle exec rake
 EOF
+  end
 end
 
 exit 1 unless passes.all?
