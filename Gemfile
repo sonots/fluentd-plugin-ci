ff55f5f483010568e0c1b6c6d9b036557db34ba8
sonots <sonots@gmail.com>
2014-05-18 15:43:40 +0900

    Support to test on branch

--- Gemfile
+++ Gemfile
@@ -1,16 +1,17 @@
 source "http://rubygems.org"
 
+gem "fluentd", git: "https://github.com/sonots/fluentd", branch: "revert_dummy_log_device_commit"
 # sonots plguins
+gem 'fluent-plugin-measure_time', git: 'https://github.com/sonots/fluent-plugin-measure_time'
+gem 'fluent-plugin-flowcounter-simple', git: 'https://github.com/sonots/fluent-plugin-flowcounter-simple'
+gem 'fluent-plugin-grep', git: 'https://github.com/sonots/fluent-plugin-grep'
 gem 'fluent-plugin-elapsed-time'
-gem 'fluent-plugin-measure_time'
 gem 'fluent-plugin-latency'
-gem 'fluent-plugin-flowcounter-simple'
 gem 'fluent-plugin-gc'
-gem 'fluent-plugin-grep'
 gem 'fluent-plugin-grepcounter'
 gem 'fluent-plugin-groupcounter'
 gem 'fluent-plugin-hash-forward'
-gem 'fluent-plugin-keep-forward'
+# gem 'fluent-plugin-keep-forward' # v0.10.47 is not yet supported
 gem 'fluent-plugin-record-reformer'
 gem 'fluent-plugin-reemit'
 gem 'fluent-plugin-stats'
@@ -28,5 +29,6 @@ gem 'fluent-plugin-datacounter'
 gem 'fluent-plugin-numeric-monitor'
 gem 'fluent-plugin-config-expander'
 gem 'fluent-plugin-amplifier-filter'
-gem 'fluent-plugin-anomalydetect'
 gem 'fluent-plugin-mail'
+gem 'fluent-plugin-anomalydetect'
+
--- run-tests.rb
+++ run-tests.rb
@@ -1,19 +1,30 @@
 require "rubygems"
 
+# get the gem fluentd version to run ci test with the version
+gem_fluentd = File.readlines('Gemfile').grep(/fluentd/).first.chomp.gsub('"', '\"')
+GEMFILE_CI = "Gemfile.fluentd_plugin_ci"
+
 specs = Gem::Specification.find_all { |s| s.name =~ /fluent-plugin/ }
-
-passes = specs.map do |spec|
+failures = []
+specs.each do |spec|
   puts "\e[31m\e[43m\e[5mRunning tests for #{spec.name}\e[0m"
-
-  Bundler.with_clean_env do
+  passed = Bundler.with_clean_env do
     system <<"EOF"
+echo "cd #{spec.full_gem_path}"
 cd #{spec.full_gem_path}
-echo "bundle install"
-bundle install
-echo "RUBYLIB=lib:test:$RUBYLIB bundle exec rake"
-RUBYLIB=lib:test:$RUBYLIB bundle exec rake
+echo 'cp Gemfile #{GEMFILE_CI} && echo "#{gem_fluentd}" >> #{GEMFILE_CI}'
+cp Gemfile #{GEMFILE_CI} && echo "#{gem_fluentd}" >> #{GEMFILE_CI}
+echo "BUNDLE_GEMFILE=#{GEMFILE_CI} bundle install"
+BUNDLE_GEMFILE=#{GEMFILE_CI} bundle install
+echo "RUBYLIB=lib:test:$RUBYLIB BUNDLE_GEMFILE=#{GEMFILE_CI} bundle exec rake"
+RUBYLIB=lib:test:$RUBYLIB BUNDLE_GEMFILE=#{GEMFILE_CI} bundle exec rake
 EOF
   end
+  failures << spec.name unless passed
 end
 
-exit 1 unless passes.all?
+failures.each do |name|
+  puts "\e[31m#{name} FAILED\e[0m"
+end
+
+exit 1 unless failures.empty?
